c9ec8ccca446e4deb6c65ab38c0d48e43f4ce2ad
Robert Casazza <rjc@oasis.nuance.com>
2023-04-16 18:09:16 -0400

    component styles with css modules

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
